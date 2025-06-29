8c9bcfa8741620733e0f83d725ce3dfcaac2f2bb
Mrimo Sorcar <msarkar07@gmail.com>
2025-06-29 17:25:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/expenses.tsx
+++ src/components/expenses.tsx
@@ -17,6 +17,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
