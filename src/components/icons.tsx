ed4e778c677747a41f2752329b4ab9a7e9f0ee72
Mrimo Sorcar <msarkar07@gmail.com>
2025-06-29 21:34:37 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,3 @@
-
 import type { SVGProps } from "react";
 
 export const EqualizeLogo = (props: SVGProps<SVGSVGElement>) => (
@@ -18,5 +17,18 @@ export const EqualizeLogo = (props: SVGProps<SVGSVGElement>) => (
 );
 
 export const Spinner = (props: SVGProps<SVGSVGElement>) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+  </svg>
 );
